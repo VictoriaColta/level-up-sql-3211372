4426a051e0e94caa54f992f0707aceac567e2dc5
VictoriaColta <65483439+VictoriaColta@users.noreply.github.com>
2023-01-27 14:57:28 +0000

    Insert into new table

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,19 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+Alter TABLE CustomersResponses
+RENAME NumberOfPeople to PartySize;
+
+SELECT * FROM Customers
+WHERE Email='atapley2j@kinetecoinc.com';
+
+SELECT CustomerID FROM Customers WHERE Email='atapley2j@kinetecoinc.com';
+
+INSERT INTO CustomersResponses (CustomerID,PartySize)
+VALUES (
+(SELECT CustomerID 
+FROM Customers 
+WHERE Email='atapley2j@kinetecoinc.com'), 3);
+
+SELECT * FROM CustomersResponses;
+
